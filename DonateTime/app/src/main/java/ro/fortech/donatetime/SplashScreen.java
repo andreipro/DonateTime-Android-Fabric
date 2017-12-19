4c01cb981314c41a56d137c19126335254171295
Andrei Prodan <andrei.prodan@fortech.ro>
2017-12-19 04:29:59 +0200

    First commit.

---
+++ DonateTime/app/src/main/java/ro/fortech/donatetime/SplashScreen.java
@@ -0,0 +1,16 @@
+package ro.fortech.donatetime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
